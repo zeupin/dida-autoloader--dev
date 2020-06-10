0d21ecd9dc264a9da53c9474b693caf305ab3ae2
Macc Liu <maccliu@qq.com>
2020-06-10 15:40:06 +0800

    提醒：修改后要同步到 dida-project 项目里面去

--- release.bat
+++ release.bat
@@ -9,4 +9,6 @@ del /f /s /q            "%TARGET_DIR%\src\*.*"
 rd /s /q                "%TARGET_DIR%\src\"
 xcopy /y /s  src        "%TARGET_DIR%\src\"
 
+echo.修改后，记得同步发布到 dida-project 项目里面去！
+
 ping -n 10 127.0.0.1>nul
